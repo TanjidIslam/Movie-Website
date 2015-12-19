21c49fd8b79b5537fa7105ae2c270063ab8a62b2
TanjidIslam <tanjid.islam@live.ca>
2015-12-18 21:50:44 -0500

    Fixed minor typo

--- media.py
+++ media.py
@@ -12,9 +12,9 @@ class Movie(object):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_img
-        self.trailer_youtube_id = trailer_url
+        self.trailer_youtube_url = trailer_url
 
     def show_trailer(self):
         '''
         '''
-        webbrowser.open(self.trailer_youtube_id)
+        webbrowser.open(self.trailer_youtube_url)
